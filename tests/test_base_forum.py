62e3fe71d6514045fcb69a9cd7f1d70621ca7c89
ye11ow <ye111111ow@gmail.com>
2018-06-16 23:09:32 -0700

    Added UT for base forum

--- tests/test_base_forum.py
+++ tests/test_base_forum.py
@@ -1,9 +1,10 @@
 from requests_html import HTML
 from tohsaka.spells.forum import BaseForumSpell
 from unittest.mock import patch
+from unittest.mock import MagicMock
 from .utils.utils import load_html
 
-class DummyR(object):
+class DummyResponse(object):
     def __init__(self, html):
         self._html = html
 
@@ -11,6 +12,13 @@ class DummyR(object):
     def html(self):
         return self._html
 
+class DummyItem(object):
+    def __init__(self, html):
+        self._html = html
+
+    @property
+    def html(self):
+        return self._html
 
 class TestForum:
 
@@ -18,7 +26,7 @@ class TestForum:
     @patch('tohsaka.spells.forum.BaseForumSpell.process_item', return_value=True)
     def test_go_page(self, process_item, HTMLSession):
         html = HTML(html=load_html('basepage'))
-        HTMLSession.return_value.get.return_value = DummyR(html)
+        HTMLSession.return_value.get.return_value = DummyResponse(html)
 
         spell = BaseForumSpell({
             'itemListSelector': '#unselect'
@@ -29,3 +37,52 @@ class TestForum:
             assert item
 
         assert True
+
+
+    @patch('tohsaka.spells.forum.HTMLSession')
+    def test_process_item(self, HTMLSession):
+        html = HTML(html=load_html('basepage'))
+        HTMLSession.return_value.get.return_value = DummyResponse(html)
+
+        spell = BaseForumSpell({
+            'titleSelector': '.title',
+            'dateSelector': '.date',
+            'contentSelector': '.description'
+        })
+
+        item = MagicMock()
+        item.absolute_links = ['test']
+        result = spell.process_item(item)
+
+        assert result['title'] == 'title'
+        assert result['pubDate'] == 'date'
+        assert result['description'] == '<div class="description">description</div>'
+        assert result['link'] == 'test'
+        assert result['addition'] == None
+
+    @patch('tohsaka.spells.forum.HTMLSession')
+    def test_process_item_wrong_selector(self, HTMLSession):
+        html = HTML(html=load_html('basepage'))
+        HTMLSession.return_value.get.return_value = DummyResponse(html)
+
+        spell = BaseForumSpell({
+            'titleSelector': '.wrongtitle',
+            'dateSelector': '.wrongdate',
+            'contentSelector': '.wrongdescription'
+        })
+
+        item = MagicMock()
+        item.absolute_links = ['test']
+        result = spell.process_item(item)
+
+        assert result == {}
+
+
+    def test_process_item_more_links(self):
+        item = MagicMock()
+        item.absolute_links = ['test', 'lin']
+
+        spell = BaseForumSpell({})
+        result = spell.process_item(item)
+
+        assert result == None
