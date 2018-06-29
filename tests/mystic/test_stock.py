c44aad0c9592ca6462eb4ffd93d95da213d7a8ce
ye11ow <ye111111ow@gmail.com>
2018-06-28 23:17:35 -0700

    Added mystic test

---
+++ tests/mystic/test_stock.py
@@ -0,0 +1,24 @@
+import tempfile
+import json
+from os.path import join as pathjoin
+from tohsaka import Tohsaka
+
+class TestStock:
+
+    def test_stock(self):
+        FILENAME = 'aapl'
+        tohsaka = Tohsaka('stock', {
+            'symbol': 'aapl',
+            'output_file': FILENAME
+        })
+
+        tohsaka.outputter.OUTPUT_FOLDER = tempfile.gettempdir()
+        tohsaka.go()
+
+
+        with open(pathjoin(tohsaka.outputter.OUTPUT_FOLDER, FILENAME + '.json'), 'r') as json_file:
+            result = json.loads(json_file.read())
+
+        assert result
+        assert 'symbol' in result[0]
+        assert 'latestPrice' in result[0]
